48f34da312bcf6eaa08c93235a722ea073174f14
Aline Maria <139500744+AlineMarias@users.noreply.github.com>
2024-04-07 08:38:31 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mysql = require('mysql2');
 const buscarRouter = require('./routes/buscar');
+
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.BCDD_HOST,
+    user: process.env.BCDD_USER,
+    port: process.env.BCDD_PORT,
+    password: process.env.BCDD_PASSWORD,
+    database: process.env.BCDD_DATABASE
+});
+
+connection.connect(err => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err);
+        return;
+    }
+    console.log('Conexão bem-sucedida ao banco de dados');
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -13,23 +32,29 @@ app.get('/', (req, res) => {
   res.render('login');
 });
 
-
 app.get('/registrar', (req, res) => {
   res.render('registrar');
 });
 
-
 app.post('/registrar', (req, res) => {
+  const { email, password } = req.body;
 
-  const { username, password } = req.body;
+  if (isValid(email, password)) {
+   
+    res.redirect('/menu');
+  } else {
  
-  res.redirect('/login');
-});
+    res.redirect('/login');
+  }
 
+});
 
 app.get('/login', (req, res) => {
   res.render('login');
 });
+
+
+app.use('/buscar', buscarRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
